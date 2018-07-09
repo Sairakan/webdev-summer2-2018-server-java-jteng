50d0c5a3e88ed8011c5abee7262798559e216369
jteng <akariass@gmail.com>
2018-07-09 15:07:44 -0400

    updating user data

--- src/main/java/com/example/webdevsummer22018serverjavajteng/services/UserService.java
+++ src/main/java/com/example/webdevsummer22018serverjavajteng/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +40,19 @@ public class UserService {
 	@PostMapping("/login")
 	public User login(@RequestBody User user) {
 		return userRepository.findUserByCredentials(user.getUsername(), user.getPassword());
+	}
+	
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(
+			@PathVariable("userId") int id,
+			@RequestBody User newUser) {
+		Optional<User> optional = userRepository.findById(id);
+		if (optional.isPresent()) {
+			User user = optional.get();
+			user.setFirstName(newUser.getFirstName());user.setLastName(newUser.getLastName());
+			return userRepository.save(user);
+		} else
+			return null;
 	}
 	
 	@GetMapping("/api/user/{userId}")
--- src/main/webapp/profile.controller.client.js
+++ src/main/webapp/profile.controller.client.js
@@ -1,6 +1,8 @@
 (function() {
 	
 	var $username, $firstName, $lastName;
+	var $updateBtn;
+	var currentUser = null;
 	
 	init();
 	
@@ -9,11 +11,31 @@
 		$username = $('#username');
 		$firstName = $('#firstName');
 		$lastName = $('#lastName');
+		$updateBtn = $('#updateBtn');
+		
+		$updateBtn.click(updateUser);
 		
 		findUserById(12).then(renderUser);
 	}
 	
+	function updateUser() {
+		var user = {
+			firstName: $firstName.val(),
+			lastName: $lastName.val()
+		};
+		
+		fetch("/api/user/" + currentUser.id, {
+			method: 'put',
+			body: JSON.stringify(user),
+			'credentials': 'include',
+			headers: {
+				'content-type': 'application/json'
+			}
+		})
+	}
+	
 	function renderUser(user) {
+		currentUser = user;
 		$username.val(user.username);
 		$firstName.val(user.firstName);
 		$lastName.val(user.lastName);
