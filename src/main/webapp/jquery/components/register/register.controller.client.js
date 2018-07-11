f87cb8c90e8749ce38c58989a49dc08d9e6680d0
jteng <akariass@gmail.com>
2018-07-11 00:23:23 -0400

    updated files and folders

--- src/main/webapp/jquery/components/register/register.controller.client.js
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -1,15 +1,26 @@
+(function () {
+    var $usernameFld, $passwordFld, $verifyPasswordFld;
+    var $registerBtn;
+    var userService = new UserService();
+    $(main);
+
+    function main() { … }
+    function register() { … }
+})();
+
+
 (function() {
-	var registerBtn = $('#registerBtn');
-	var usernameFld = $('#username');
-	var passwordFld = $('#password');
-	var password2Fld = $('#password2');
+	var $registerBtn = $('#registerBtn');
+	var $usernameFld = $('#username');
+	var $passwordFld = $('#password');
+	var $password2Fld = $('#password2');
 	
 	registerBtn.click(registerHandler);
 	
 	function registerHandler() {
-		var usernameStr = usernameFld.val();
-		var passwordStr = passwordFld.val();
-		var password2Str = password2Fld.val();
+		var usernameStr = $usernameFld.val();
+		var passwordStr = $passwordFld.val();
+		var password2Str = $password2Fld.val();
 		
 		var userObj = {
 			username: usernameStr,
