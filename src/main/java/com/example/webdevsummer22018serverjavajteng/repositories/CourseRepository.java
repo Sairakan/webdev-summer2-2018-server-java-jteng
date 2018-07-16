27155a6fe7c2d3a2e7fb9455814cc7e13bb2900c
jteng <akariass@gmail.com>
2018-07-16 10:45:03 -0400

    created content for course server

---
+++ src/main/java/com/example/webdevsummer22018serverjavajteng/repositories/CourseRepository.java
@@ -0,0 +1,15 @@
+package com.example.webdevsummer22018serverjavajteng.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.webdevsummer22018serverjavajteng.models.Course;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+	
+}
---
+++ src/main/java/com/example/webdevsummer22018serverjavajteng/services/CourseService.java
@@ -0,0 +1,58 @@
+package com.example.webdevsummer22018serverjavajteng.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdevsummer22018serverjavajteng.models.Course;
+import com.example.webdevsummer22018serverjavajteng.models.User;
+import com.example.webdevsummer22018serverjavajteng.repositories.CourseRepository;
+
+public class CourseService {
+	
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@PostMapping("/api/course")
+	public Course createCourse(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+	@DeleteMapping("/api/course/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		courseRepository.deleteById(id);
+	}
+	
+	@GetMapping("/api/course")
+	public List<Course> findAllCourses() {
+		return (List<Course>) courseRepository.findAll();
+	}
+	
+	@GetMapping("/api/user/{id}")
+	public Optional<Course> findCourseByUserId(@PathVariable("id") String id) {
+		return courseRepository.findById(Integer.parseInt(id));
+	}
+	
+	@PutMapping("/api/course/{id}")
+	public Course updateUser(@PathVariable("id") String id, @RequestBody Course course) {
+		Optional<Course> opt = courseRepository.findById(Integer.parseInt(id));
+		if (opt.isPresent()) {
+			Course currentCourse = opt.get();
+			currentCourse.setTitle(course.getTitle());
+			currentCourse.setOwner(course.getOwner());
+			currentCourse.setModified(course.getModified());
+			currentCourse.setCreated(course.getCreated());
+			
+			return courseRepository.save(currentCourse);
+		} else return null;
+	}
+}
