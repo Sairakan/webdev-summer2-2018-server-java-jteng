c34edd47c2d6130484a23a2f79a75d5e051819d0
jteng <akariass@gmail.com>
2018-07-17 10:44:36 -0400

    added request handlers to ModuleService

--- src/main/java/wbdv/services/ModuleService.java
+++ src/main/java/wbdv/services/ModuleService.java
@@ -1,5 +1,6 @@
 package wbdv.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.*;
@@ -29,5 +30,16 @@ public class ModuleService {
 			return moduleRepository.save(newModule);
 		} else return null;
 	}
-
+	@GetMapping("/api/course/{courseId}/module")
+	public List<Module> findAllModulesForCourse(@PathVariable("courseId") int courseId) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if (data.isPresent()) {
+			Course course = data.get();
+			return course.getModules();
+		} else return null;
+	}
+	@DeleteMapping("/api/module/{moduleId}")
+	public void deleteModule(@PathVariable("moduleId") int moduleId) {
+		moduleRepository.deleteById(moduleId);
+	}
 }
